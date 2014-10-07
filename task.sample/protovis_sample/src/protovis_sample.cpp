efbfcc46cd2d5006620aaee1cab00a9e280ffd4d
Anton Grigoryev <me@ansgri.com>
2014-10-07 23:30:23 +0400

    NEW: protovis_sample: a scaffolding for the Protovis demo program

---
+++ task.sample/protovis_sample/src/protovis_sample.cpp
@@ -0,0 +1,36 @@
+#include <protovis/lineplot.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdexcept>
+#include <cstdio>
+
+
+namespace rsdt { namespace protovis_sample {
+
+void main(int argc, char **argv)
+{
+  if (argc < 2)
+    throw std::runtime_error("wrong number of arguments");
+
+  cv::Mat src = cv::imread(argv[1]);
+  cv::imshow("src", src);
+  while ((cv::waitKey(100) & 0xFF) != 27)
+  {
+    // nothing
+  }
+}
+
+}}
+
+int main(int argc, char **argv)
+{
+  try
+  {
+    rsdt::protovis_sample::main(argc, argv);
+    return 0;
+  }
+  catch (std::exception const& e)
+  {
+    fprintf(stderr, "Exception: %s\n", e.what());
+    return 1;
+  }
+}
