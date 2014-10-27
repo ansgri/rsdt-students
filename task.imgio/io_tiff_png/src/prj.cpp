7b0531c0a5034a7f0893502c0429fbb8d3b6e7d4
alexpashevich <alexpashevich@github.com>
2014-10-28 00:27:18 +0300

    prj.cpp

--- task.imgio/io_tiff_png/src/prj.cpp
+++ task.imgio/io_tiff_png/src/prj.cpp
@@ -1,5 +1,5 @@
 #include "tiffio.h"
-#include <png.h>
+#include "png.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -19,55 +19,55 @@ int writeToPNG(png_byte *buf, uint32 height, uint32 width, const char *path)
         printf("error during the opening of png file\n");
         return status;
     }
-//    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-//    if (png_ptr == NULL)
-//    {
-//        printf("error during png_create_write_struct\n");
-//        return status;
-//    }
-//    info_ptr = png_create_info_struct(png_ptr);
-//    if (info_ptr == NULL) {
-//        printf("error during png_create_info_struct\n");
-//        return status;
-//    }
-//    if (setjmp(png_jmpbuf(png_ptr))) {
-//        printf("error during setjmp\n");
-//        return status;
-//    }
-//    png_set_IHDR(png_ptr,
-//                 info_ptr,
-//                 width,
-//                 height,
-//                 depth,
-//                 PNG_COLOR_TYPE_RGB,
-//                 PNG_INTERLACE_NONE,
-//                 PNG_COMPRESSION_TYPE_DEFAULT,
-//                 PNG_FILTER_TYPE_DEFAULT);
-//    
-//    row_pointers = (png_byte**)png_malloc(png_ptr, height * sizeof(png_byte*));
-//    for (uint32 y = 0; y < height; ++y) {
-//        png_byte *row = (png_byte*)png_malloc(png_ptr, sizeof (png_byte) * width * pixel_size);
-//        row_pointers[y] = row;
-//        for (uint32 x = 0; x < width; ++x) {
-//            png_byte pixel = buf[y * height + x]; // kill x3 below
-//            *row++ = pixel;
-//            *row++ = pixel;
-//            *row++ = pixel;
-//        }
-//    }
-//    
-//    png_init_io (png_ptr, fp);
-//    png_set_rows (png_ptr, info_ptr, row_pointers);
-//    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-//    
-//    status = 0;
-//    
-//    for (uint32 y = 0; y < height; ++y) {
-//        png_free(png_ptr, row_pointers[y]);
-//    }
-//    png_free(png_ptr, row_pointers);
-//    
-//    png_destroy_write_struct(&png_ptr, &info_ptr);
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL)
+    {
+        printf("error during png_create_write_struct\n");
+        return status;
+    }
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL) {
+        printf("error during png_create_info_struct\n");
+        return status;
+    }
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        printf("error during setjmp\n");
+        return status;
+    }
+    png_set_IHDR(png_ptr,
+                 info_ptr,
+                 width,
+                 height,
+                 depth,
+                 PNG_COLOR_TYPE_RGB,
+                 PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT,
+                 PNG_FILTER_TYPE_DEFAULT);
+    
+    row_pointers = (png_byte**)png_malloc(png_ptr, height * sizeof(png_byte*));
+    for (uint32 y = 0; y < height; ++y) {
+        png_byte *row = (png_byte*)png_malloc(png_ptr, sizeof (png_byte) * width * pixel_size);
+        row_pointers[y] = row;
+        for (uint32 x = 0; x < width; ++x) {
+            png_byte pixel = buf[y * height + x]; // kill x3 below
+            *row++ = pixel;
+            *row++ = pixel;
+            *row++ = pixel;
+        }
+    }
+    
+    png_init_io (png_ptr, fp);
+    png_set_rows (png_ptr, info_ptr, row_pointers);
+    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+    
+    status = 0;
+    
+    for (uint32 y = 0; y < height; ++y) {
+        png_free(png_ptr, row_pointers[y]);
+    }
+    png_free(png_ptr, row_pointers);
+    
+    png_destroy_write_struct(&png_ptr, &info_ptr);
     fclose(fp);
     return status;
 }
