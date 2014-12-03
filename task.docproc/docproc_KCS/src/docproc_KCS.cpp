619d62fd629502c567410542d4a10e2b3e598293
mastov <a.mastov@ya.ru>
2014-12-03 19:12:33 +0300

    FIX:autocontrast is better now

--- task.docproc/docproc_KCS/src/docproc_KCS.cpp
+++ task.docproc/docproc_KCS/src/docproc_KCS.cpp
@@ -5,38 +5,36 @@ using cv::Size;
 
 int main(int argc, char const** argv)
 {
-    Mat src,src2, dst, dst2;
-    std::string image_name = "/home/mastov/rsdt-students/bin.linux64.release/2.jpg";
-    std::string out_image_name = "/home/mastov/rsdt-students/bin.linux64.release/out.jpg";
-    std::string out_image_name1 = "/home/mastov/rsdt-students/bin.linux64.release/out1.jpg";
+  if (!argv[1])
+  {
+    printf("You need to specify the input file\n");
+    return 1;
+  }
+  if (!argv[2])
+  {
+    printf("You need to specify the output file\n");
+    return 1;
+  }
+  Mat src, erode_pic, dilate_pic;
+  std::string image_name = argv[1] ;
+  std::string out_image_name = argv[2];
 
-    src = cv::imread(image_name, CV_LOAD_IMAGE_GRAYSCALE);
-    //cv::resize(src, src, Size(),0.2, 0.2);
-    int BLUR_APERTURE = 100;
-    Mat const strel = cv::getStructuringElement(0, Size(BLUR_APERTURE, BLUR_APERTURE));
+  src = cv::imread(image_name, CV_LOAD_IMAGE_GRAYSCALE);
 
-    cv::morphologyEx(src, dst, cv::MORPH_ERODE, strel);
-    cv::morphologyEx(src, dst2, cv::MORPH_DILATE, strel);
-    Mat ret2, ret3;
-    // !!!!
-    cv::threshold(dst2-dst,ret2, 50,255, 3); // applaying threshold to max-min difference. get drawing with "border"
-    cv::threshold(dst2-dst,ret3, 50,255, 2); // white list
-    // !!!!
-    dst.convertTo(dst, CV_32FC1);
-    dst2.convertTo(dst2, CV_32FC1);
-    ret2.convertTo(ret2, CV_32FC1);
-    ret3.convertTo(ret3, CV_32FC1);
+  int BLUR_APERTURE = 100;
+  Mat const strel = cv::getStructuringElement(0, Size(BLUR_APERTURE, BLUR_APERTURE));
 
-    src.convertTo(src, CV_32FC1);
+  cv::morphologyEx(src, erode_pic, cv::MORPH_ERODE, strel);
+  cv::morphologyEx(src, dilate_pic, cv::MORPH_DILATE, strel);
+  cv::threshold(erode_pic,erode_pic, 50, 50, 2);
 
-    Mat ret = (src-dst)/(dst2-dst); // old variant ,local auto contrast: brightet pxls -> white, darkest -> black
-    Mat ret_new = (src-dst)/(ret2+0.001*ret3); // new. make list white, and does nothing with drawing
-    ret = 255*ret;
+  erode_pic.convertTo(erode_pic, CV_32FC1);
+  dilate_pic.convertTo(dilate_pic, CV_32FC1);
+  src.convertTo(src, CV_32FC1);
 
-    ret_new = 255*ret_new;
+  Mat out = (src - erode_pic)/(dilate_pic - erode_pic);
+  out = 255*out;
+  cv::imwrite(out_image_name, out);
 
-    //cv::imwrite(out_image_name, ret); //old
-    cv::imwrite(out_image_name, ret_new); // new
-
-    return 0;
+  return 0;
 }
