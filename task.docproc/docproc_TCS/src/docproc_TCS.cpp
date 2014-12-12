082c4b005db89872d32bdeb3fe031f3ee9a70da1
jshemiakina <jshemiakina@gmail.com>
2014-12-12 18:10:40 +0400

    ADD: skeletonization

--- task.docproc/docproc_TCS/src/docproc_TCS.cpp
+++ task.docproc/docproc_TCS/src/docproc_TCS.cpp
@@ -19,11 +19,11 @@ void autocontrast(char const* src_path, char const* dst_path)
 
   cv::erode(image, er_img, element);
   cv::dilate(image, dil_img, element);
-
+    
+  image.convertTo(image, CV_32FC1);
   er_img.convertTo(er_img, CV_32FC1);
   dil_img.convertTo(dil_img, CV_32FC1);
-  image.convertTo(image, CV_32FC1);
-
+ 
   double min = 0;
   cv::minMaxLoc(dil_img, &min);
 
@@ -35,13 +35,48 @@ void autocontrast(char const* src_path, char const* dst_path)
   cv::imwrite(dst_path, res);
 }
 
+void sketetonization(char const* src_path, char const* dst_path)
+{
+  cv::Mat image = cv::imread(src_path, CV_LOAD_IMAGE_GRAYSCALE);
+  if (!image.data)
+    throw std::invalid_argument("Error when read the file");
+
+  cv::Mat background;
+  cv::medianBlur(image, background, 201);
+  image = background - image;
+
+  cv::threshold(image, image, 0.0, 255.0, cv::THRESH_OTSU);
+
+  cv::Mat skelet(image.size(), CV_8UC1, cv::Scalar(0));
+  cv::Mat tmp(image.size(), CV_8UC1);
+
+  const cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+
+  while (true)
+  {     
+    cv::morphologyEx(image, tmp, cv::MORPH_OPEN, element);
+    skelet = skelet | (image - tmp);
+    double max = 0;
+    cv::erode(image, image, element);
+    cv::minMaxLoc(image, 0, &max);
+    if (max == 0)
+      break;
+  }
+
+  cv::imwrite(dst_path, 255 - skelet);
+}
+
 int main(int argc, const char** argv)
 {
-  if (argc != 3)
+  if (argc != 4)
+  {
     std::cout << "Invalid arguments.\n";
+    return 0;
+  }
   try
   {
     autocontrast(argv[1], argv[2]);
+    sketetonization(argv[1], argv[3]);
   }
   catch(std::exception e)
   { std::cout << e.what() << std::endl; }
