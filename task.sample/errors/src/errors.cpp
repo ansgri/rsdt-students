9583bc67242478fef104f818bdcf4792166e248b
Anton Grigoryev <me@ansgri.com>
2015-02-10 13:33:20 +0300

    ENH: errors: run with valgrind --tool=memcheck --track-origins=yes --leak-check=full ./errors

--- task.sample/errors/src/errors.cpp
+++ task.sample/errors/src/errors.cpp
@@ -1,15 +1,16 @@
 #include <cstdio>
 
+int x;
 
 int main(int argc, char ** argv)
 {
-  int a[10];
+  int a[10] = {0};
   // int b[5] = {0};
   int * c = new int[10];
 
-
   printf("a[10] = %d\n", a[10]);
   printf("c[5] = %d\n", c[5]);
+  printf("x = %d\n", x);
   // printf("c[10] = %d\n", c[10]);
 
   return 0;
