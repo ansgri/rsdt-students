02ebe4d0a06c7f933baa7279e9da640abc177217
alexpashevich <alexpashevich@github.com>
2015-05-21 00:08:20 +0300

    task 2 and 3 done by pashevich

--- task.keypoints/pashevich/src/touristless.cpp
+++ task.keypoints/pashevich/src/touristless.cpp
@@ -4,6 +4,7 @@
 #include "opencv2/nonfree/features2d.hpp"
 #include <vector>
 #include <utility>
+#include <dirent.h>
 
 using namespace std;
 using namespace cv;
@@ -17,67 +18,67 @@ void show_image(Mat const& src) {
   cv::destroyWindow("image");
 }
 
-void printVect(string name, vector<pair<Point2i, Point2i> > vect) {
+void print_vect(string name, vector<pair<Point2i, Point2i> > vect) {
   cout << name << endl;
   for (int i = 0; i < vect.size(); ++i) {
     cout << "[[" << vect[i].first.x << ", " << vect[i].first.y << "], [" << vect[i].second.x << ", " << vect[i].second.y << "]]" << endl;
   }
 }
-void printRects(pair<Point2i, Point2i> rect1, pair<Point2i, Point2i> rect2) {
+void print_rects(pair<Point2i, Point2i> rect1, pair<Point2i, Point2i> rect2) {
   cout << "introsecting" << endl;
   cout << "[[" << rect1.first.x << ", " << rect1.first.y << "], [" << rect1.second.x << ", " << rect1.second.y << "]]" << endl;
   cout << "[[" << rect2.first.x << ", " << rect2.first.y << "], [" << rect2.second.x << ", " << rect2.second.y << "]]" << endl;
 }
 
-vector<vector<pair<Point2i, Point2i> > > readTouristLocations(string sourceFileName, int n) {
-  ifstream toursitLocationsFile;
-  toursitLocationsFile.open(sourceFileName);
-  vector<vector<pair<Point2i, Point2i> > > toursitLocations;
-  if (!toursitLocationsFile.is_open()) {
+vector<vector<pair<Point2i, Point2i> > > read_tourist_locations(string source_file_name, int n) {
+  ifstream toursit_locations_file;
+  toursit_locations_file.open(source_file_name);
+  vector<vector<pair<Point2i, Point2i> > > toursit_locations;
+  if (!toursit_locations_file.is_open()) {
     cout << "Can not get tourist locations" << endl;
-    return toursitLocations;
+    return toursit_locations;
   }
 
   for (int i = 0; i < n; ++i) {
-    vector<pair<Point2i, Point2i> > curLocations;
-    int numberOfRects;
-    toursitLocationsFile >> numberOfRects;
-    for (int j = 0; j < numberOfRects; ++j) {
+    vector<pair<Point2i, Point2i> > cur_locations;
+    int number_of_rects;
+    toursit_locations_file >> number_of_rects;
+    for (int j = 0; j < number_of_rects; ++j) {
       int x1, x2, y1, y2;
-      toursitLocationsFile >> x1 >> y1 >> x2 >> y2;
-      curLocations.push_back(make_pair(Point2i(x1 * 4, y1 * 4), Point2i(x2 * 4, y2 * 4)));
+      toursit_locations_file >> x1 >> y1 >> x2 >> y2;
+      cur_locations.push_back(make_pair(Point2i(x1 * 4, y1 * 4), Point2i(x2 * 4, y2 * 4)));
     }
-    toursitLocations.push_back(curLocations);
+    toursit_locations.push_back(cur_locations);
   }
 
-  return toursitLocations;
+  return toursit_locations;
 }
 
-vector<pair<Point2i, Point2i> > transformTouristLocations(vector<pair<Point2i, Point2i> > origin, Mat homography) {
-  // printVect("origin", origin);
-  vector<pair<Point2i, Point2i> > newLocations;
+vector<pair<Point2i, Point2i> > transform_tourist_locations(vector<pair<Point2i, Point2i> > origin, Mat homography) {
+  // print_vect("origin", origin);
+  vector<pair<Point2i, Point2i> > new_locations;
   for (int i = 0; i < origin.size(); ++i) {
     pair<Point2i, Point2i> curPair = origin[i];
-    vector<Point2f> pointsVector;
-    pointsVector.push_back(origin[i].first);
-    pointsVector.push_back(cvPoint(origin[i].second.x, origin[i].first.y));
-    pointsVector.push_back(cvPoint(origin[i].first.x, origin[i].second.y));
-    pointsVector.push_back(origin[i].second);
-    vector<Point2f> outputPoints(4);
-    perspectiveTransform(pointsVector, outputPoints, homography);
+    vector<Point2f> points_vector;
+    points_vector.push_back(origin[i].first);
+    points_vector.push_back(cvPoint(origin[i].second.x, origin[i].first.y));
+    points_vector.push_back(cvPoint(origin[i].first.x, origin[i].second.y));
+    points_vector.push_back(origin[i].second);
+    vector<Point2f> output_points(4);
+    perspectiveTransform(points_vector, output_points, homography);
 
-    Point2i pUL = cvPoint(static_cast<int>(outputPoints[0].x), static_cast<int>(outputPoints[0].y));
+    Point2i pUL = cvPoint(static_cast<int>(output_points[0].x), static_cast<int>(output_points[0].y));
     Point2i pLR = pUL;
-    for (int k = 1; k < outputPoints.size(); ++k) {
-      if (pUL.x > static_cast<int>(outputPoints[k].x)) pUL.x = static_cast<int>(outputPoints[k].x);
-      if (pUL.y > static_cast<int>(outputPoints[k].y)) pUL.y = static_cast<int>(outputPoints[k].y);
-      if (pLR.x < static_cast<int>(outputPoints[k].x)) pLR.x = static_cast<int>(outputPoints[k].x);
-      if (pLR.y < static_cast<int>(outputPoints[k].y)) pLR.y = static_cast<int>(outputPoints[k].y);
+    for (int k = 1; k < output_points.size(); ++k) {
+      if (pUL.x > static_cast<int>(output_points[k].x)) pUL.x = static_cast<int>(output_points[k].x);
+      if (pUL.y > static_cast<int>(output_points[k].y)) pUL.y = static_cast<int>(output_points[k].y);
+      if (pLR.x < static_cast<int>(output_points[k].x)) pLR.x = static_cast<int>(output_points[k].x);
+      if (pLR.y < static_cast<int>(output_points[k].y)) pLR.y = static_cast<int>(output_points[k].y);
     }
-    newLocations.push_back(make_pair(pUL, pLR));
+    new_locations.push_back(make_pair(pUL, pLR));
   }
-  // printVect("newLocations", newLocations);
-  return newLocations;
+  // print_vect("new_locations", new_locations);
+  return new_locations;
 }
 
 bool introsect(pair<Point2i, Point2i> rect1, pair<Point2i, Point2i> rect2) {
@@ -89,105 +90,124 @@ bool introsect(pair<Point2i, Point2i> rect1, pair<Point2i, Point2i> rect2) {
   }
 }
 
-int touristlessization(string inputFolderName, int n, int objectId, string outputFolderName, vector<vector<pair<Point2i, Point2i> > > touristLocations) {
-  Mat imgObject = imread(inputFolderName + to_string(objectId) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
-  Mat imgObjectCol = imread(inputFolderName + to_string(objectId) + ".jpg");
+int get_number_of_files(const char *path) {
+  int n = 0;
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir(path)) != NULL) {
+      while ((ent = readdir(dir)) != NULL) {
+          if (ent->d_name[0] != '.') {
+              ++n;
+              // cv::Mat img = cv::imread(std::string(folder) + ent->d_name, CV_LOAD_IMAGE_GRAYSCALE);
+              // vect.push_back(std::make_pair(label, img));
+          }
+      }
+      closedir (dir);
+  } else {
+      std::cout << "Can not open directory " << path << std::endl;
+  }
+  return n;
+}
+
+int touristlessization(string input_folder_name, int n, int object_id, string output_folder_name, vector<vector<pair<Point2i, Point2i> > > tourist_locations) {
+  Mat img_object = imread(input_folder_name + to_string(object_id) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
+  Mat img_object_col = imread(input_folder_name + to_string(object_id) + ".jpg");
 
   SiftFeatureDetector detector(0, 4, 0.04, 10, 1.6);
   SiftDescriptorExtractor extractor(3.0);
 
-  std::vector<KeyPoint> keypointsOfObject;
-  detector.detect(imgObject, keypointsOfObject);
+  std::vector<KeyPoint> keypoints_of_object;
+  detector.detect(img_object, keypoints_of_object);
 
-  Mat descriptorsOfObject;
-  extractor.compute(imgObject, keypointsOfObject, descriptorsOfObject);
-  vector<vector<pair<Point2i, Point2i> > > newTouristLocations(n);
-  newTouristLocations[objectId - 1] = touristLocations[objectId - 1];
+  Mat descriptors_of_object;
+  extractor.compute(img_object, keypoints_of_object, descriptors_of_object);
+  vector<vector<pair<Point2i, Point2i> > > new_tourist_locations(n);
+  new_tourist_locations[object_id - 1] = tourist_locations[object_id - 1];
 
-  if (descriptorsOfObject.empty()) {
+  if (descriptors_of_object.empty()) {
     cout << "object descriptors error\n";
     return -1;
   }
 
   for (int i = 1; i <= n; ++i) {
-    if (i == objectId) {
+    if (i == object_id) {
       continue;
     }
-    std::vector<KeyPoint> keypointsOfScene;
-    Mat descriptorsOfScene;
+    std::vector<KeyPoint> keypoints_of_scene;
+    Mat descriptors_of_scene;
 
-    Mat imgScene = imread(inputFolderName + to_string(i) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat imgSceneCol = imread(inputFolderName + to_string(i) + ".jpg");
+    Mat img_scene = imread(input_folder_name + to_string(i) + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat img_scene_col = imread(input_folder_name + to_string(i) + ".jpg");
 
-    detector.detect(imgScene, keypointsOfScene);
-    extractor.compute(imgScene, keypointsOfScene, descriptorsOfScene);
+    detector.detect(img_scene, keypoints_of_scene);
+    extractor.compute(img_scene, keypoints_of_scene, descriptors_of_scene);
 
-    if (descriptorsOfScene.empty()) {
+    if (descriptors_of_scene.empty()) {
       cout << "scene descriptors error\n";
       return -1;
     }
 
     FlannBasedMatcher matcher;
     vector<DMatch> matches;
-    matcher.match(descriptorsOfObject, descriptorsOfScene, matches);
+    matcher.match(descriptors_of_object, descriptors_of_scene, matches);
 
-    double maxDist = 0;
-    double minDist = 100;
+    double max_dist = 0;
+    double min_dist = 100;
 
-    for (int j = 0; j < descriptorsOfObject.rows; ++j) {
+    for (int j = 0; j < descriptors_of_object.rows; ++j) {
       double dist = matches[j].distance;
-      if (dist < minDist)
-        minDist = dist;
-      if (dist > maxDist)
-        maxDist = dist;
+      if (dist < min_dist)
+        min_dist = dist;
+      if (dist > max_dist)
+        max_dist = dist;
     }
 
-    vector<DMatch> bestMatches;
-    for (int j = 0; j < descriptorsOfObject.rows; ++j) {
-      if (matches[j].distance < 3 * minDist) {
-        bestMatches.push_back(matches[j]);
+    vector<DMatch> best_matches;
+    for (int j = 0; j < descriptors_of_object.rows; ++j) {
+      if (matches[j].distance < 3 * min_dist) {
+        best_matches.push_back(matches[j]);
       }
     }
 
     vector<Point2f> obj;
     vector<Point2f> scene;
-    for (int j = 0; j < bestMatches.size(); ++j) {
-      obj.push_back(keypointsOfObject[bestMatches[j].queryIdx].pt);
-      scene.push_back(keypointsOfScene[bestMatches[j].trainIdx].pt);
+    for (int j = 0; j < best_matches.size(); ++j) {
+      obj.push_back(keypoints_of_object[best_matches[j].queryIdx].pt);
+      scene.push_back(keypoints_of_scene[best_matches[j].trainIdx].pt);
     }
 
     Mat homography = findHomography(obj, scene, CV_RANSAC);
-    newTouristLocations[i - 1] = transformTouristLocations(touristLocations[i - 1], homography);
+    new_tourist_locations[i - 1] = transform_tourist_locations(tourist_locations[i - 1], homography);
 
-    warpPerspective(imgSceneCol, imgSceneCol, homography.inv(), imgObject.size());
-    imwrite(outputFolderName + to_string(i) + ".jpg", imgSceneCol);
+    warpPerspective(img_scene_col, img_scene_col, homography.inv(), img_object.size());
+    imwrite(output_folder_name + to_string(i) + ".jpg", img_scene_col);
     cout << "Image number " << i << " done\n";
   }
-  imwrite(outputFolderName + to_string(objectId) + ".jpg", imgObjectCol);
+  imwrite(output_folder_name + to_string(object_id) + ".jpg", img_object_col);
 
-  vector<Mat> imagesNormalizedCol;
+  vector<Mat> images_normalized_col;
   for (int i = 1; i <= n; ++i) {
-    Mat imageCol = imread(outputFolderName + to_string(i) + ".jpg");
-    imagesNormalizedCol.push_back(imageCol);
+    Mat image_col = imread(input_folder_name + to_string(i) + ".jpg");
+    images_normalized_col.push_back(image_col);
   }
 
-  for (int i = 0; i < newTouristLocations[objectId - 1].size(); ++i) {
+  for (int i = 0; i < new_tourist_locations[object_id - 1].size(); ++i) {
     // пройти по всем картинкам - если на одной из них в данном прямоугольнике нету туристов, то просто вырезаем из неё кусок
-    bool imgFound = false;
+    bool img_found = false;
     int curimg = 0;
-    while (!imgFound && curimg < n) {
-      if (curimg != objectId - 1) {
+    while (!img_found && curimg < n) {
+      if (curimg != object_id - 1) {
         int j = 0;
         bool introsecting = false;
-        while (!introsecting && j < newTouristLocations[curimg].size()) {
-          if (introsect(newTouristLocations[objectId - 1][i], newTouristLocations[curimg][j])) {
-            // printRects(newTouristLocations[objectId - 1][i], newTouristLocations[curimg][j]);
+        while (!introsecting && j < new_tourist_locations[curimg].size()) {
+          if (introsect(new_tourist_locations[object_id - 1][i], new_tourist_locations[curimg][j])) {
+            // print_rects(new_tourist_locations[object_id - 1][i], new_tourist_locations[curimg][j]);
             introsecting = true;
           }
           ++j;
         }
         if (!introsecting) {
-          imgFound = true;
+          img_found = true;
         } else {
           ++curimg;
         }
@@ -196,42 +216,56 @@ int touristlessization(string inputFolderName, int n, int objectId, string outpu
       }
 
     }
-    if (!imgFound) {
+    if (!img_found) {
       cout << "Can not found image to replace tourists number " << i << endl;
       // если нету такого прямоугольника, то надо запускать какой-то рекурсивный алгоритм, где мы вырезаем кусок из этого прямоугольника
       // а потом делаем то же самое для того что получилось по рекурсии, при этом говорим, что одно из изображений мы уже использовали
     } else {
       cout << "Replacing tourists number " << i << " with " << curimg + 1 << ".jpg" << endl;
-      int left = newTouristLocations[objectId - 1][i].first.x;
-      int top = newTouristLocations[objectId - 1][i].first.y;
-      int width = newTouristLocations[objectId - 1][i].second.x - newTouristLocations[objectId - 1][i].first.x;
-      int height = newTouristLocations[objectId - 1][i].second.y - newTouristLocations[objectId - 1][i].first.y;
-      Mat a = imagesNormalizedCol[curimg](Rect(left, top, width, height));
-      a.copyTo(imgObjectCol(Rect(left, top, width, height)));
+      int left = new_tourist_locations[object_id - 1][i].first.x;
+      int top = new_tourist_locations[object_id - 1][i].first.y;
+      int width = new_tourist_locations[object_id - 1][i].second.x - new_tourist_locations[object_id - 1][i].first.x;
+      int height = new_tourist_locations[object_id - 1][i].second.y - new_tourist_locations[object_id - 1][i].first.y;
+      Mat a = images_normalized_col[curimg](Rect(left, top, width, height));
+      a.copyTo(img_object_col(Rect(left, top, width, height)));
     }
   }
-  imwrite(outputFolderName + "touristless" + to_string(objectId) + ".jpg", imgObjectCol);
+  imwrite(output_folder_name + "touristless" + to_string(object_id) + ".jpg", img_object_col);
+  cout << "touristlessization done" << endl;
   return 0;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc < 4 || argc > 5) {
-    cout << "Usage: path to folder with images, number of images, number of the best photo, [output folder]\n";
+  if (argc != 5) {
+    cout << "Wrong number of arguments. Given " << (argc - 1) << ", expected 4\n";
+    cout << "Usage:" << endl;
+    cout << "1) path to input folder with images" << endl;
+    cout << "2) number of the best photo" << endl;
+    cout << "3) path to tourist coordinates file (.txt)" << endl;
+    cout << "4) path to output folder" << endl;
     return -1;
   }
-  string inputFolderName = argv[1]; //"../testdata/occlusion_01/images/";
-  int n = stoi(argv[2]);
-  int objectId = stoi(argv[3]);
-  string outputFolderName = (argc == 5 ? argv[4] : "../testdata/occlusion_01/images/output/");
-  string toursitLocationsFileName = "../testdata/occlusion_01/coordinates.txt";
+  string input_folder_name = argv[1]; // "../testdata/occlusion_01/images/";
+  int object_id = stoi(argv[2]);
+  string toursit_locations_filename = argv[3]; // "../testdata/occlusion_01/coordinates.txt";
+  string output_folder_name = argv[4]; // "../testdata/occlusion_01/images/output/");
+  if (input_folder_name[input_folder_name.length() - 1] != '/') {
+    input_folder_name.append("/");
+  }
+  if (output_folder_name[output_folder_name.length() - 1] != '/') {
+    output_folder_name.append("/");
+  }
+
+  int n = get_number_of_files(input_folder_name.c_str());
 
   if (n < 2) {
-    cout << "Need more images\n";
-    return - 1;
+    cout << "Not enough images for touristlessization. Given " << n << endl;
   }
-  vector<vector<pair<Point2i, Point2i> > > touristLocations = readTouristLocations(toursitLocationsFileName, n);
-  if (touristLocations.size() > 0) {
-    touristlessization(inputFolderName, n, objectId, outputFolderName, touristLocations);
+
+  cout << "Starting to tourislessify" << endl;
+  vector<vector<pair<Point2i, Point2i> > > tourist_locations = read_tourist_locations(toursit_locations_filename, n);
+  if (tourist_locations.size() > 0) {
+    touristlessization(input_folder_name, n, object_id, output_folder_name, tourist_locations);
   } else {
     cout << "Can not read tourist locations" << endl;
   }
--- task.ml/char_rec/src/task3.cpp
+++ task.ml/char_rec/src/task3.cpp
@@ -3,29 +3,30 @@
 #include <vector>
 #include <algorithm>
 #include <stdio.h>
+#include <dirent.h>
 
 #include "opencv2/opencv.hpp"
 
 #include "fann.h"
 #include "floatfann.h"
 
-void readJPGs(std::vector<std::pair<char, cv::Mat> >& vect, int start_num, int end_num, char label, const char *folder) {
-    for (int i = start_num; i < end_num; ++i) {
-        std::string num = std::to_string(i);
-        if (num.length() < 3) {
-            for (int j = 0; j <= 3 - num.length(); ++j) {
-                num.insert(0, "0");
+void read_jpgs(std::vector<std::pair<std::string, cv::Mat> >& vect, std::string label, const char *folder) {
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(folder)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_name[0] != '.') {
+                cv::Mat img = cv::imread(std::string(folder) + ent->d_name, CV_LOAD_IMAGE_GRAYSCALE);
+                vect.push_back(std::make_pair(label, img));
             }
         }
-        std::string filename = "_00" + num + ".jpg";
-        filename.insert(0, 1, label);
-        filename.insert(0, folder);
-        cv::Mat img = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-        vect.push_back(std::make_pair(label, img));
+        closedir (dir);
+    } else {
+        std::cout << "Can not open directory " << folder << std::endl;
     }
 }
 
-void writeMatToFile(cv::Mat& m, std::ofstream& fout) {
+void write_mat_to_file(cv::Mat& m, std::ofstream& fout) {
     if (!fout) {
         std::cout << "File Can Not Be Opened" << std::endl;
         return;
@@ -39,78 +40,111 @@ void writeMatToFile(cv::Mat& m, std::ofstream& fout) {
     fout << std::endl;
 }
 
-void createData(const char *filename,
-                int pixel_number,
-                std::pair<int, int> first_num,
-                std::pair<int, int> second_num,
-                char first_label,
-                char second_label,
-                const char *folder_name) {
+void create_data(std::string input_folder,
+                 std::string filename,
+                 int pixel_number,
+                 std::string first_label,
+                 std::string second_label
+                ) {
     std::ofstream file(filename, std::ofstream::out);
-    std::vector<std::pair<char, cv::Mat> > vect;
-    readJPGs(vect, first_num.first, first_num.second, first_label, folder_name);
-    readJPGs(vect, first_num.first, first_num.second, second_label, folder_name);
+    std::vector<std::pair<std::string, cv::Mat> > vect;
+    read_jpgs(vect, first_label, (input_folder + first_label + "/").c_str());
+    read_jpgs(vect, second_label, (input_folder + second_label + "/").c_str());
     std::random_shuffle(vect.begin(), vect.end());
 
     file << vect.size() << " " << pixel_number << " " << 1 << std::endl;
     for (int i = 0; i < vect.size(); ++i) {
-        writeMatToFile(vect[i].second, file);
+        write_mat_to_file(vect[i].second, file);
         file << (vect[i].first == first_label ? 0 : 1) << std::endl;
-        // 'X' -> 0
-        // 'Y' -> 1
     }
     file.close();
 }
 
-
-int main() {
-    // creation of train.txt and test.txt file
-
-    int pixel_number = 21 * 28;
-    createData("train.txt", pixel_number, std::make_pair(0, 100), std::make_pair(0, 100), 'X', 'Y', "train/");
-    createData("test.txt", pixel_number, std::make_pair(100, 188), std::make_pair(100, 199), 'X', 'Y', "test/");
-
-
-
-
-    // training NN
+void train_nn(std::string output_folder, int pixel_number, const unsigned int num_epochs) {
     const unsigned int num_input = pixel_number;
     const unsigned int num_output = 1;
     const unsigned int num_layers = 3;
     const unsigned int num_neurons_hidden = 400;
-    const unsigned int num_epochs = 1000;
     
     struct fann *ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
-    fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
-    fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
+    fann_set_activation_function_hidden(ann, FANN_SIGMOID);
+    fann_set_activation_function_output(ann, FANN_SIGMOID);
 
     struct fann_train_data *train_data, *test_data;
-    train_data = fann_read_train_from_file("train.txt");
-    test_data = fann_read_train_from_file("test.txt");
+    train_data = fann_read_train_from_file((output_folder + "train.txt").c_str());
+    test_data = fann_read_train_from_file((output_folder + "test.txt").c_str());
 
-    std::vector<float> mse_train;
-    std::vector<float> mse_test;
+    std::vector<float> err_rate_train;
+    std::vector<float> err_rate_test;
+    fann_type *calc_out;
+    int calc_out_int;
 
     for (int i = 0; i < num_epochs; ++i) {
         std::cout << "training epoch number " << i << std::endl;
         fann_train_epoch(ann, train_data);
-        mse_train.push_back(fann_test_data(ann, train_data));
-        mse_test.push_back(fann_test_data(ann, test_data));
+        int err_num = 0;
+        for (unsigned int j = 0; j < train_data->num_data; ++j) {
+            calc_out = fann_run(ann, train_data->input[j]);
+            calc_out_int = (int)round(calc_out[0]);
+            // std::cout << "Real value = " << train_data->output[j][0] << ", answer = " << calc_out_int << std::endl;
+            if (calc_out_int != train_data->output[j][0]) {
+                ++err_num;
+            }
+        }
+        err_rate_train.push_back(1.0 * err_num / train_data->num_data);
+
+        err_num = 0;
+        for (unsigned int j = 0; j < test_data->num_data; ++j) {
+            calc_out = fann_test(ann, test_data->input[j], test_data->output[j]);
+            calc_out_int = (int)round(calc_out[0]);
+            // std::cout << "Real value = " << test_data->output[j][0] << ", answer = " << calc_out_int << std::endl;
+            if (calc_out_int != test_data->output[j][0]) {
+                ++err_num;
+            }
+        }
+        err_rate_test.push_back(1.0 * err_num / test_data->num_data);
     }
 
     fann_destroy(ann);
-    std::ofstream output_test("output_test.txt", std::ofstream::out);
-    std::ofstream output_train("output_train.txt", std::ofstream::out);
+    std::ofstream output_test(output_folder + "output_test.txt", std::ofstream::out);
+    std::ofstream output_train(output_folder + "output_train.txt", std::ofstream::out);
 
-    for (int i = 0; i < mse_train.size(); ++i) {
-        // printf("\nTrain error: %f, Test error: %f\n", mse_train[i], mse_test[i]);
-        output_test << mse_test[i] << std::endl;
-        output_train << mse_train[i] << std::endl;
+    for (int i = 0; i < num_epochs; ++i) {
+        // printf("Train error: %f, Test error: %f\n", err_rate_train[i], err_rate_test[i]);
+        output_test << err_rate_train[i] << std::endl;
+        output_train << err_rate_test[i] << std::endl;
     }
-    return 0;
 }
 
+int main(int argc, char* argv[]) {
+    if (argc != 6) {
+        std::cout << "Wrong number of arguments. Given " << (argc - 1) << ", expected 5\n";
+        std::cout << "Usage:" << std::endl;
+        std::cout << "1) path to input folder with test and train folders from archives" << std::endl;
+        std::cout << "2) first symbol" << std::endl;
+        std::cout << "3) second symbol" << std::endl;
+        std::cout << "4) path to output folder" << std::endl;
+        std::cout << "5) number of epochs to train" << std::endl;
+        return -1;
+    }
 
+    std::string input_folder = argv[1];
+    std::string char1 = argv[2];
+    std::string char2 = argv[3];
+    std::string output_folder = argv[4];
+    unsigned int num_epochs = atoi(argv[5]);
 
+    if (input_folder[input_folder.length() - 1] != '/') {
+        input_folder.append("/");
+    }
+    if (output_folder[output_folder.length() - 1] != '/') {
+        output_folder.append("/");
+    }
 
+    int pixel_number = 21 * 28;
+    create_data(input_folder + "train/", output_folder + "train.txt", pixel_number, char1, char2);
+    create_data(input_folder + "test/", output_folder + "test.txt", pixel_number, char1, char2);
 
+    train_nn(output_folder, pixel_number, num_epochs);
+    return 0;
+}
